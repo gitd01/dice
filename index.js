07c9f7e3cfb8069977a6007914bec3842c30c7ef
gitd01 <110153941+gitd01@users.noreply.github.com>
2022-11-17 16:07:16 +0530

    edit

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var img_src2="images/dice"+random_no2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",img_src2);
 
 if(random_no1>random_no2){
-    document.querySelector("h1").textContent="Player-1 won the match.🚩"
+    document.querySelector("h3").textContent="Player-1 won the match!"
 }
 else if(random_no1<random_no2){
-    document.querySelector("h1").textContent="Player-2 won the match.🚩"
+    document.querySelector("h3").textContent="Player-2 won the match!"
 }
 else{
-    document.querySelector("h1").textContent="Draw!."
+    document.querySelector("h3").textContent="Draw!"
 }
